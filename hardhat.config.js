57d110c5cde719b802934de9f1e5f57ff8df9251
Paulo Henrique Silveira <paulo.silveira@gabriel.com.br>
2023-05-06 13:05:28 -0300

    first commit

---
+++ hardhat.config.js
@@ -0,0 +1,32 @@
+require("@nomiclabs/hardhat-waffle");
+const dotenv = require('dotenv');
+dotenv.config();
+
+const GOERLI_URL = process.env.GOERLI_URL
+const SEPOLIA_URL = process.env.SEPOLIA_URL
+const MUMBAI_URL = process.env.MUMBAI_URL
+const PRIVATE_KEY = process.env.PRIVATE_KEY
+const API_KEY = process.env.API_KEY
+
+module.exports = {
+  solidity: "0.8.1",
+  etherscan: {
+    // Sua chave API key do Etherscan
+    // Obtenha a sua em https://etherscan.io/
+    apiKey: API_KEY,
+  },
+  networks: {
+    goerli: {
+      url: GOERLI_URL,
+      accounts: [PRIVATE_KEY]
+    },
+    sepolia: {
+      url: SEPOLIA_URL,
+      accounts: [PRIVATE_KEY]
+    },
+    mumbai: {
+      url: MUMBAI_URL,
+      accounts: [PRIVATE_KEY]
+    }
+  },
+};
---
+++ scripts/deploy.js
@@ -0,0 +1,28 @@
+const main = async () => {
+  const nftContractFactory = await hre.ethers.getContractFactory("MyEpicNFT");
+  const nftContract = await nftContractFactory.deploy();
+  await nftContract.deployed();
+  console.log("Contrato implantado em:", nftContract.address);
+
+  // Chama a função.
+  let txn = await nftContract.makeAnEpicNFT({
+    gasPrice: ethers.utils.parseUnits("20", "gwei"),
+  });
+  // Espera ela ser minerada.
+  await txn.wait();
+  console.log("Cunhou NFT #1");
+  txn = await nftContract.makeAnEpicNFT();
+  // Espera ela ser minerada.
+  await txn.wait();
+  console.log("Cunhou NFT #2");
+};
+const runMain = async () => {
+  try {
+    await main();
+    process.exit(0);
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+};
+runMain();
